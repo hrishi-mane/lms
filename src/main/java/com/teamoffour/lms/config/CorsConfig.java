bfbd5e7160dc43688b4b78d2eab8d2002fe4bd05
hrishi-mane <manehrishikesh229@gmail.com>
2026-04-24 15:21:38 +0100

    Adding one more origin port in CORS.

--- src/main/java/com/teamoffour/lms/config/CorsConfig.java
+++ src/main/java/com/teamoffour/lms/config/CorsConfig.java
@@ -6,11 +6,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/lms/**")
-                .allowedOrigins("http://localhost:5173")  // your frontend port
-                .allowedMethods("GET", "POST", "DELETE", "PUT")
-                .allowedHeaders("*");
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://localhost:5173"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
